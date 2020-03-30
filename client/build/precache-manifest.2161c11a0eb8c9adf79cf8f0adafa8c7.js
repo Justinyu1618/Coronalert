14ff4f371a5a93719d3f6a8284bca86988865e4c
Justin Yu <justinyu1618@gmail.com>
2020-03-30 06:10:19 -0400

    temp added build

---
+++ client/build/precache-manifest.2161c11a0eb8c9adf79cf8f0adafa8c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b259eb2ce30287d168e5809231541d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647288ed5a284f03b292",
+    "url": "/static/css/2.5d84600f.chunk.css"
+  },
+  {
+    "revision": "cd3c2eb6a5d0a07d50e6",
+    "url": "/static/css/main.fc6990ad.chunk.css"
+  },
+  {
+    "revision": "647288ed5a284f03b292",
+    "url": "/static/js/2.5179b8e3.chunk.js"
+  },
+  {
+    "revision": "414aaca732bcd516cd15350363a8ed7c",
+    "url": "/static/js/2.5179b8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3c2eb6a5d0a07d50e6",
+    "url": "/static/js/main.b7d6832f.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

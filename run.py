073c74c30eb8e7fa78e11eb9b5f084799882dc4f
Justin Yu <justinyu1618@gmail.com>
2020-03-30 06:14:25 -0400

    run.py change

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from server import create_app
+import os
 
 if __name__ == "__main__":
     app = create_app()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)

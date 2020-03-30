fe40b33f38601c02c670c99654e60730167cd837
Justin Yu <justinyu1618@gmail.com>
2020-03-30 18:40:19 -0400

    temp build folder for Heroku deploy

---
+++ client/build/precache-manifest.6ab879760ae2437e445ca50acb81a20d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a900118846e12071447a6c4d5d6bc6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ea0a6f419219a54eaf",
+    "url": "/static/css/2.5d84600f.chunk.css"
+  },
+  {
+    "revision": "d0d234bd1769fd479263",
+    "url": "/static/css/main.6db06221.chunk.css"
+  },
+  {
+    "revision": "24ea0a6f419219a54eaf",
+    "url": "/static/js/2.672866b3.chunk.js"
+  },
+  {
+    "revision": "414aaca732bcd516cd15350363a8ed7c",
+    "url": "/static/js/2.672866b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d234bd1769fd479263",
+    "url": "/static/js/main.b8458065.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
